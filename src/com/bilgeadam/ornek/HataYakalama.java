ee2e827433b6ae1846ca0c04eec3f1228f16e2a6
kmsync <tommyjwi@gmail.com>
2021-12-03 01:23:28 +0300

    hata yakalama işlendi

--- src/com/bilgeadam/ornek/HataYakalama.java
+++ src/com/bilgeadam/ornek/HataYakalama.java
@@ -5,21 +5,24 @@ public class HataYakalama {
     public static void main(String[] args) {
 
         try {
-
             int sayi1 = 7;
             int sayi2 = 0;
 
             double sonuc = sayi1 / sayi2;
 
             System.out.println("Sonuç: " + sonuc);
+        } catch (ArithmeticException ae) {
+            System.err.println("HATA! Sıfıra bölme hatası! ");
+        } catch (Exception e) {
+            // serr kısayolu ile System.err.println kullanarak yazdırıyoruz.
+            // Çünkü çıktı rengi hataya uygun olarak kırmızı olarak veriliyor.
+            System.err.println("Hata Oluştu!" + " " + e.getMessage());
+            System.out.println("*****************************");
 
+            // Bu kodu verilen hatanın türünü görmek için kullanabiliriz.
+            e.printStackTrace();
         }
-        catch (Exception e)
-        {
-            System.out.println("Hata Oluştu!");
-            System.err.println("Hata Oluştu!");
-        }
+
     }
-
 
 }
